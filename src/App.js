ccadacb91439d42dfdeef09f05c2f4d6d7d7c675
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-07 18:51:36 +0900

    added state to todo data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from "react";
 import TodoItem from "./TodoItem";
 import todosData from "./todosData";
 
-function App() {
-  const todosComponent = todosData.map((item) => (
-    <TodoItem key={item.id} data={item} />
-  ));
-  return <div className="todo-list">{todosComponent}</div>;
+// function App() {
+//   const todosComponent = todosData.map((item) => (
+//     <TodoItem key={item.id} data={item} />
+//   ));
+//   return <div className="todo-list">{todosComponent}</div>;
+// }
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todosData,
+    };
+  }
+  render() {
+    const todoItems = this.state.todosData.map((item) => (
+      <TodoItem key={item.id} item={item} />
+    ));
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
