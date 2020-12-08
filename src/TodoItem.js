f0ea64cb85525eeb46c06ec289c5a55a296fc094
Takuhei Mizutani <takuheim1011@gmail.com>
2020-12-08 13:31:19 +0900

    added conditional rendering

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,24 @@
-import React from "react"
+import React from "react";
 
 function TodoItem(props) {
-    return (
-        <div className="todo-item">
-            <input
-                type="checkbox"
-                checked={props.item.completed}
-                onChange={() => props.handleChange(props.item.id)}
-            />
-            <p>{props.item.text}</p>
-        </div>
-    )
+  const completedStyle = {
+    fontStyle: "italic",
+    color: "#cdcdcd",
+    textDecoration: "line-through",
+  };
+
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p style={props.item.completed ? completedStyle : null}>
+        {props.item.text}
+      </p>
+    </div>
+  );
 }
 
-export default TodoItem
+export default TodoItem;
